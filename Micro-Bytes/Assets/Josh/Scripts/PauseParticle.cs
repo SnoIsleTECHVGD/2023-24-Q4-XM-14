7f77789b35c6240dcf6b05b8353c67bd33117b6c
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-03-21 13:26:11 -0700

    I tried working on some particles.

--- Micro-Bytes/Assets/Josh/Scripts/PauseParticle.cs
+++ Micro-Bytes/Assets/Josh/Scripts/PauseParticle.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseParticle : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+   
     void Start()
     {
+     ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Stop();
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.tag.Equals("Generator"))
+        {
+            ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+            particleSystem.Play();
+            if (particleSystem.totalTime == 0.51)
+            {
+                particleSystem.Pause();
+
+            }
+        }
+       
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Generator"))
+        {
+            ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+            particleSystem.Stop();
+        }
+      
     }
 }
