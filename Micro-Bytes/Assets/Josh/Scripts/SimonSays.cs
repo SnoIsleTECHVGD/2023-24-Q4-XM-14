7a80745aa559eef1154e279b0c7e5ab27372d86b
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-04-10 13:27:06 -0700

    More stuff done, almost got the simon says minigame completed

---
+++ Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimonSays : MonoBehaviour
+{
+    
+    GameObject Button1;
+    GameObject Button2;
+    GameObject Button3;
+    GameObject Button4;
+    // Update is called once per frame
+    void Update()
+    {
+        Button1.GetComponent<Button>();
+        Button2.GetComponent<Button>();
+        Button3.GetComponent<Button>();
+        Button4.GetComponent<Button>();
+       
+    }
+}
