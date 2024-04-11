d0704c188ba4903889519df8ad1363b0b42398c3
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-04-11 13:25:45 -0700

    idk michael marmalade activities or smth

--- Micro-Bytes/Assets/Ethan/Scripts/MichaelMarmaladeTheManipulatedMicroBitsMaliciousMovementManuscript.cs
+++ Micro-Bytes/Assets/Ethan/Scripts/MichaelMarmaladeTheManipulatedMicroBitsMaliciousMovementManuscript.cs
@@ -1,36 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class MichaelMarmaladeTheManipulatedMicroBitsMaliciousMovementManuscript : MonoBehaviour
 {
+    //STOLE THIS FROM JOSH, GONNA TWEAK IT A BIT TOO
+
     //The speed the object is moving at.
     public float speed = 1.0f;
 
     //The thing our object is moving to.
-    public GameObject @byte;
-    public Transform targets;
     public GameObject blacklights;
+    public Transform targets;
+
+    //public GameObject current;
+
+
     void Start()
     {
         //Gets our object's transform.
-        @byte = GameObject.FindWithTag("player");
-        targets = @byte.transform;
+        blacklights = GameObject.FindWithTag("Blacklight");
+        targets = blacklights.transform;
     }
+
 
     private void OnTriggerStay2D(Collider2D other)
     {
+
         if (other.gameObject.CompareTag("Blacklight"))
         {
+
             transform.position = Vector2.MoveTowards(transform.position, blacklights.transform.position, speed);
-            transform.position = Vector2.MoveTowards(transform.position, @byte.transform.position, 0);
             this.GetComponent<pointTowards>().enabled = true;
-        }
-        else 
-        {
-            transform.position = Vector2.MoveTowards(transform.position, blacklights.transform.position, 0);
-            transform.position = Vector2.MoveTowards(transform.position, @byte.transform.position, speed);
-            this.GetComponent<pointTowards>().enabled = false;
+            this.GetComponent<MichaelMarmaladesMaliciousMachineMovementManuscript>().enabled = false;
         }
 
     }
@@ -40,8 +43,10 @@ public class MichaelMarmaladeTheManipulatedMicroBitsMaliciousMovementManuscript 
         if (other.gameObject.CompareTag("Blacklight"))
         {
             transform.position = Vector2.MoveTowards(transform.position, blacklights.transform.position, 0);
-            transform.position = Vector2.MoveTowards(transform.position, @byte.transform.position, 0);
             this.GetComponent<pointTowards>().enabled = false;
+            this.GetComponent<MichaelMarmaladesMaliciousMachineMovementManuscript>().enabled = true;
         }
+
     }
 }
+
