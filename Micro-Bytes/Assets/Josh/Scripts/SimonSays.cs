008fe6dd299a8f5b8b2420bb2134475a4f8af2b4
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-04-11 11:48:33 -0700

    Pushing so Andrew can work on level 2 while I work on some code.

--- Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
+++ Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class SimonSays : MonoBehaviour
 {
-    
-    GameObject Button1;
-    GameObject Button2;
-    GameObject Button3;
-    GameObject Button4;
+    public GameObject Button1;
+    public GameObject Button2;
+    public GameObject Button3;
+    public GameObject Button4;
     // Update is called once per frame
     void Update()
     {
-        Button1.GetComponent<Button>();
-        Button2.GetComponent<Button>();
-        Button3.GetComponent<Button>();
-        Button4.GetComponent<Button>();
+        GameObject[] Buttons;
        
     }
 }
