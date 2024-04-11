bb9c3bab232506bee420669fb82f05fc91fd87db
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-04-11 13:25:07 -0700

    Almost completely finished Simon Says, but I'm like one step away.

--- Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
+++ Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SimonSays : MonoBehaviour
@@ -8,10 +10,31 @@ public class SimonSays : MonoBehaviour
     public GameObject Button2;
     public GameObject Button3;
     public GameObject Button4;
+    public Sprite ButtonChange;
     // Update is called once per frame
     void Update()
     {
-        GameObject[] Buttons;
-       
+        GameObject[] Buttons = {Button1, Button2, Button3, Button4};
+        int i = 0;
+        if (Button1)
+        {
+
+        }
+        /* 
+          if (Buttons[0].GetComponent<SpriteRenderer>().sprite == ButtonChange)
+        {
+            if (Buttons[1].GetComponent<SpriteRenderer>().sprite == ButtonChange)
+            {
+                if (Buttons[2].GetComponent<SpriteRenderer>().sprite == ButtonChange)
+                {
+                    if (Buttons[3].GetComponent<SpriteRenderer>().sprite == ButtonChange)
+                    {
+                        this.gameObject.SetActive(false);
+                    }
+
+                }
+            }
+        } 
+       */
     }
 }
