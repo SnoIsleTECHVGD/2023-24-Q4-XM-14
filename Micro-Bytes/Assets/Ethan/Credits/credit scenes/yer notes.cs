1b8697fe628925552fb04df3bc7a350f75f173df
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-04-16 12:33:26 -0700

    prolly sumthing in credits

---
+++ Micro-Bytes/Assets/Ethan/Credits/credit scenes/yer notes.cs
@@ -0,0 +1,13 @@
+ /*Ethan credits:
+  * Camera Follow
+  * enemy movement
+  * player movement
+  * speedometer (for code)
+  * SFX design/implementation
+  * 
+  *Toby credits:
+  * Animation Implementation
+  * Puzzle mechanics
+  * Healthbar 
+  * 
+  */
