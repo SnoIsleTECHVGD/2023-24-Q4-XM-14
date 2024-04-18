821d06b8e042ac3e4e9f10a92886b3f1c4be092d
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-04-18 13:15:05 -0700

    I would say a naughty word but Im not going to. I hate Simon Says.

--- Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
+++ Micro-Bytes/Assets/Josh/Scripts/SimonSays.cs
@@ -11,10 +11,11 @@ public class SimonSays : MonoBehaviour
     public GameObject Button3;
     public GameObject Button4;
     public Sprite ButtonChange;
+    public Sprite ButtonChanger;
     // Update is called once per frame
     void Update()
     {
-       
+        
         /* int i = 0;
          if (Button1)
          {
@@ -25,20 +26,57 @@ public class SimonSays : MonoBehaviour
        List<GameObject> Buttoner = new List<GameObject>() {Button1, Button2, Button3, Button4};
 
 
-       /* foreach (GameObject Button in Buttoner)
+        foreach (GameObject Button in Buttoner)
         {
-
-            if (Button.CompareTag("Pushed Down"))
+            if (Button1.GetComponent<SpriteRenderer>().sprite == ButtonChange)
             {
-                if ()
+                if (Button1.gameObject.GetComponent<SpriteRenderer>().sprite == ButtonChanger)
                 {
-                    this.gameObject.SetActive(false);
+                    this.gameObject.SetActive(true);
                 }
-       
+                if (Button2.GetComponent<SpriteRenderer>().sprite == ButtonChange)
+                {
+                    if (Button2.gameObject.GetComponent<SpriteRenderer>().sprite == ButtonChanger)
+                    {
+                        this.gameObject.SetActive(true);
+                    }
+                    if (Button3.GetComponent<SpriteRenderer>().sprite == ButtonChange)
+                    {
+                        if (Button3.gameObject.GetComponent<SpriteRenderer>().sprite == ButtonChanger)
+                        {
+                            this.gameObject.SetActive(true);
+                        }
+                        if (Button4.GetComponent<SpriteRenderer>().sprite == ButtonChange)
+                        {
+                            this.gameObject.SetActive(false);
+                            if (Button4.gameObject.GetComponent<SpriteRenderer>().sprite == ButtonChanger)
+                            {
+                                this.gameObject.SetActive(true);
+                            }
+                        }
+                        else
+                        {
+                            this.gameObject.SetActive(true);
+                        }
+                    }
+                    else
+                    {
+                        this.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    this.gameObject.SetActive(true);
+                }
             }
-        }   */
+            else
+            { 
+             this.gameObject.SetActive(true);
+            }
+        }   
 
     }
+
 
 
 
