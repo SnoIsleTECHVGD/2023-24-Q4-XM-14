4136796783dca570fd7c508522ac2f8b24860fa4
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-04-24 12:36:05 -0700

    Indubitably (more sfx)

--- Micro-Bytes/Assets/Ethan/Scripts/byte_pointer.cs
+++ Micro-Bytes/Assets/Ethan/Scripts/byte_pointer.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class byte_pointer : MonoBehaviour
 {
-    
+    public Transform Target;
+    public float HideDistance;
+    private void Update()
+    {
+        var dir = Target.position - transform.position;
+        if(dir.magnitude < HideDistance)
+        {
+            SetChildrenActive(false);
+        }
+        else
+        {
+            SetChildrenActive(true);
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+    }
+    void SetChildrenActive(bool value)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+        }
+    }
 }
