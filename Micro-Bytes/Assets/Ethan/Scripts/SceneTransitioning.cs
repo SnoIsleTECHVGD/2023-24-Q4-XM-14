5d28aa547e65f05daf5dbbe5f591ca3f926f720f
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-04-30 13:18:46 -0700

    transitional stuff

--- Micro-Bytes/Assets/Ethan/Scripts/SceneTransitioning.cs
+++ Micro-Bytes/Assets/Ethan/Scripts/SceneTransitioning.cs
@@ -7,14 +7,6 @@ public class SceneTransitioning : MonoBehaviour
 {
     public Animator transition;
     public float TransitionTime = 1;
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            LoadNextLevel();
-        }
-    }
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
