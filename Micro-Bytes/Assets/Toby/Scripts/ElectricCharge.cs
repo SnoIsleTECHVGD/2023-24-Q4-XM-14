fb1f85a526b08f4b0e860c28ed6b17e51a9ea84f
cookiecattornado <1719296@mukilteo.wednet.edu>
2024-05-02 12:32:40 -0700

    Ok now it should be ready for playtesting

--- Micro-Bytes/Assets/Toby/Scripts/ElectricCharge.cs
+++ Micro-Bytes/Assets/Toby/Scripts/ElectricCharge.cs
@@ -11,22 +11,33 @@ public class ElectricCharge : MonoBehaviour
     public bool immune;
     public void OnCollisionEnter2D(Collision2D other)
     {
-        if (this.gameObject.CompareTag("Generator") && other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Michael") && isCharged == false)
+        if (this.gameObject.CompareTag("Generator") && other.gameObject.CompareTag("Player") && isCharged == false)
         {
             isCharged = true;
             //immune = true;
         }
-        else if (this.gameObject.CompareTag("Circuit Box") && other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Michael") && isCharged == true)
+        else if (this.gameObject.CompareTag("Generator") && other.gameObject.CompareTag("Michael") && isCharged == false)
         {
-            onBoxPress.Invoke();
-            isCharged = false;
-            //immune = false;
+            isCharged = true;
         }
-        else if (this.gameObject.CompareTag("Timed Box") && other.gameObject.CompareTag("Player") || this.gameObject.CompareTag("Timed Box") && other.gameObject.CompareTag("Michael") && isCharged == true)
+        if (this.gameObject.CompareTag("Timed Box") && other.gameObject.CompareTag("Michael") && isCharged == true)
         {
             isCharged = false;
             timerGenerator.GetComponent<TimedGenerator>().isOn = true;
             timerGenerator.GetComponent<TimedGenerator>().PowerOn();
         }
+        else if (this.gameObject.CompareTag("Timed Box") && other.gameObject.CompareTag("Player") && isCharged == true)
+        {
+            isCharged = false;
+            timerGenerator.GetComponent<TimedGenerator>().isOn = true;
+            timerGenerator.GetComponent<TimedGenerator>().PowerOn();
+        }
+        else if (this.gameObject.CompareTag("Circuit Box") && other.gameObject.CompareTag("Player") && isCharged == true)
+        {
+            onBoxPress.Invoke();
+            isCharged = false;
+            //immune = false;
+        }
+
     }
-}
+} 
