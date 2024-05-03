b080329c2922982de73cc179ba9ac92b49e3ee80
MUKILTEO\1717222 <1717222@mukilteo.wednet.edu>
2024-05-03 13:04:30 -0700

    coll

--- Micro-Bytes/Assets/Toby/Scripts/UnityEventOnCollideOrCollision.cs
+++ Micro-Bytes/Assets/Toby/Scripts/UnityEventOnCollideOrCollision.cs
@@ -8,6 +8,8 @@ public class UnityEventOnCollideOrCollision : MonoBehaviour
     
     public UnityEvent collide;
     public UnityEvent onCollision;
+    public bool MichaelBox;
+    public GameObject Box;
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -18,7 +20,15 @@ public class UnityEventOnCollideOrCollision : MonoBehaviour
         {
             other.tag = "Player";
         }
-        
+        if (this.CompareTag("Red Box") && other.CompareTag("Player") && MichaelBox == true)
+        {
+            Box.GetComponent<Rigidbody2D>().mass = 1000;
+        }
+        if (this.CompareTag("Red Box") && other.CompareTag("Michael") && MichaelBox == true)
+        {
+            Box.GetComponent<Rigidbody2D>().mass = 10;
+        }
+
     }
     public void OnCollisionEnter2D(Collision2D other)
     {
